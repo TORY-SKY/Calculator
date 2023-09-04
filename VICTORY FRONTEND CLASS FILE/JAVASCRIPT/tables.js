9e4a356c4edc48608ae5399764f24f51251a1e6c
TORY-SKY <ezevictor559@gmail.com>
2023-09-04 04:05:18 -0700

    today_with_git

--- VICTORY FRONTEND CLASS FILE/JAVASCRIPT/tables.js
+++ VICTORY FRONTEND CLASS FILE/JAVASCRIPT/tables.js
@@ -1,23 +1,4 @@
-
-
-//working with inputs
-try{
-const logInBTN = document.querySelector('#loginButton');
-logInBTN.addEventListener('click', ()=>{
-    alert('the button has been clicked');
-    const testing12 = document.createElement('h1')
-testing12.textContent ="this is your line:" + " " + document.querySelector('#formF').firstChild.value;
-if(document.querySelector('#formF').firstChild.value == ""){
-    alert('empty input bro');
-    return false;
-}else{
-
-document.getElementById('loginDetails').appendChild(testing12);
-console.log(testing12);
-}
-});
-
-}catch (error){
-    
-    console.error('bro, try harder:', error.message);
-}
+const createH1tag = document.createElement('h1');
+createH1tag.textContent = 'trythis out Brandy';
+document.querySelector('.loginDetails').appendChild(createH1tag);
+console.log(createH1tag);

2059f14caaede843e3e73fbc2bc03fbb009d1a5e
TORY-SKY <ezevictor559@gmail.com>
2023-09-05 07:02:07 -0700

    workinon_javaScript

--- VICTORY FRONTEND CLASS FILE/JAVASCRIPT/tables.js
+++ VICTORY FRONTEND CLASS FILE/JAVASCRIPT/tables.js
@@ -1,16 +1,13 @@
 
-
+/*
 function buttn(){
     const lis = document.querySelectorAll('li');
 lis.forEach(element => {
     const parag = document.createElement('li');
-    const addThis = prompt('enter your name bro');
-    const node = document.createTextNode(addThis);
+    const addThis = document.getElementById('userName');
+    const node = document.createTextNode(addThis.value);
     parag.appendChild(node);
-    if(addThis === ' '){
-        
-        return false
-    }
+    
    
         element.appendChild(parag);
     
@@ -18,3 +15,26 @@ lis.forEach(element => {
 }
 const addBTN = document.getElementById('addBTN');
 addBTN.addEventListener('click', buttn);
+*/
+document.getElementById('saveBtn').addEventListener('click', () => {
+    const whatTodo = document.querySelector('#todoItems');
+    localStorage.setItem('todo', whatTodo.value);
+    //getting saved todos
+    const getItems = localStorage.getItem('todo');
+    //parent node ul
+    const displayTodo = document.querySelector('#addTodo');
+    //li tage to display the todo
+    const createLi = document.createElement('li');
+    createLi.textContent = getItems;
+    displayTodo.appendChild(createLi);
+
+
+
+
+})
+function remove() {
+
+    const check = localStorage.getItem('todo');
+    console.log(check);
+}
+remove();

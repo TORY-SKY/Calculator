21f5746e1558cbeb6f7afa8e8ec8f96aa4db3063
TORY-SKY <ezevictor559@gmail.com>
2023-09-08 12:01:43 -0700

    feat: Implement dynamic city selection based on the chosen state
    This commit introduces JavaScript code that allows users to select a state from a dropdown menu. Upon selecting a state, the code dynamically populates another dropdown menu with cities specific to that state.
    
    The key features of this code include:
    - Retrieving the selected state from the dropdown menu.
    - Clearing the city options when a new state is selected.
    - Populating the city dropdown menu with cities corresponding to the selected state (e.g., Lagos in this case).
    This functionality enhances the user experience by providing relevant city options based on the chosen state, making it easier to navigate and select cities.
    
    This code demonstrates a practical use of JavaScript event listeners and dynamic DOM manipulation, which can be valuable for building interactive web applications.

--- VICTORY FRONTEND CLASS FILE/state_citesPROJ/JS/states_cities.js
+++ VICTORY FRONTEND CLASS FILE/state_citesPROJ/JS/states_cities.js
@@ -1,46 +1,288 @@
-const selectedOptions = document.getElementById('states');
-console.log(selectedOptions.value);
+const nigeriaCities = {
+  FCT_Abuja: [
+    "Abuja",
+    "TANGAYIKA",
+    "Im SUCCESSFUL"
+  ],
+  Abia: [
+    "Umuahia",
+    "Aba",
+    "Arochukwu",
+    "Ohafia",
+    "Bende",
+  ],
+  Adamawa: [
+    "Yola",
+    "Mubi",
+    "Jimeta",
+    "Ganye",
+    "Numan",
+  ],
+  AkwaIbom: [
+    "Uyo",
+    "Eket",
+    "Ikot Ekpene",
+    "Oron",
+    "Abak",
+  ],
+  Anambra: [
+    "Awka",
+    "Onitsha",
+    "Nnewi",
+    "Aguata",
+    "Ekwusigo",
+  ],
+  Bauchi: [
+    "Bauchi",
+    "Azare",
+    "Misau",
+    "Jama'are",
+    "Katagum",
+  ],
+  Bayelsa: [
+    "Yenagoa",
+    "Sagbama",
+    "Brass",
+    "Ogbia",
+    "Nembe",
+  ],
+  Benue: [
+    "Makurdi",
+    "Gboko",
+    "Otukpo",
+    "Katsina-Ala",
+    "Vandeikya",
+  ],
+  Borno: [
+    "Maiduguri",
+    "Biu",
+    "Monguno",
+    "Damboa",
+    "Gwoza",
+  ],
+  CrossRiver: [
+    "Calabar",
+    "Ugep",
+    "Ikom",
+    "Ogoja",
+    "Obubra",
+  ],
+  Delta: [
+    "Asaba",
+    "Warri",
+    "Sapele",
+    "Ughelli",
+    "Agbor",
+  ],
+  Ebonyi: [
+    "Abakaliki",
+    "Afikpo",
+    "Onueke",
+    "Ishieke",
+    "Ohaukwu",
+  ],
+  Edo: [
+    "Benin City",
+    "Auchi",
+    "Ekpoma",
+    "Igueben",
+    "Uromi",
+  ],
+  Ekiti: [
+    "Ado-Ekiti",
+    "Ikere-Ekiti",
+    "Efon-Alaaye",
+    "Ode-Ekiti",
+    "Ikole-Ekiti",
+  ],
+  Enugu: [
+    "Enugu",
+    "Nsukka",
+    "Agbani",
+    "Nkwo Nike",
+    "Awgu",
+  ],
+  Gombe: [
+    "Gombe",
+    "Kumo",
+    "Dukku",
+    "Nafada",
+    "Deba",
+  ],
+  Imo: [
+    "Owerri",
+    "Orlu",
+    "Okigwe",
+    "Mbaise",
+    "Nkwerre",
+  ],
+  Jigawa: [
+    "Dutse",
+    "Hadejia",
+    "Kazaure",
+    "Birnin Kudu",
+    "Gumel",
+  ],
+  Kaduna: [
+    "Kaduna",
+    "Zaria",
+    "Kafanchan",
+    "Soba",
+    "Jema'a",
+  ],
+  Kano: [
+    "Kano",
+    "Kano Municipal",
+    "Fagge",
+    "Gwale",
+    "Dala",
+  ],
+  Katsina: [
+    "Katsina",
+    "Daura",
+    "Kankia",
+    "Malumfashi",
+    "Funtua",
+  ],
+  Kebbi: [
+    "Birnin Kebbi",
+    "Sokoto",
+    "Argungu",
+    "Yauri",
+    "Jega",
+  ],
+  Kogi: [
+    "Lokoja",
+    "Idah",
+    "Okene",
+    "Kabba",
+    "Ankpa",
+  ],
+  Kwara: [
+    "Ilorin",
+    "Offa",
+    "Omu-Aran",
+    "Kaiama",
+    "Jebba",
+  ],
+  Lagos: [
+    "Ikeja",
+    "Lagos Island",
+    "Victoria Island",
+    "Apapa",
+    "Surulere",
+    "Lekki",
+    "Ajah",
+    "Ikorodu",
+    "Badagry",
+    "Banana Island",
+  ],
+  Nasarawa: [
+    "Lafia",
+    "Keffi",
+    "Akwanga",
+    "Nasarawa",
+    "Wamba",
+  ],
+  Niger: [
+    "Minna",
+    "Bida",
+    "Suleja",
+    "Kontagora",
+    "New Bussa",
+  ],
+  Ogun: [
+    "Abeokuta",
+    "Sagamu",
+    "Ijebu-Ode",
+    "Ilaro",
+    "Ota",
+  ],
+  Ondo: [
+    "Akure",
+    "Ondo",
+    "Owo",
+    "Ikare",
+    "Okitipupa",
+  ],
+  Osun: [
+    "Osogbo",
+    "Ile-Ife",
+    "Ilesa",
+    "Ede",
+    "Ila-Orangun",
+  ],
+  Oyo: [
+    "Ibadan",
+    "Ogbomoso",
+    "Iseyin",
+    "Saki",
+    "Eruwa",
+  ],
+  Plateau: [
+    "Jos",
+    "Bukuru",
+    "Pankshin",
+    "Langtang",
+    "Shendam",
+  ],
+  Rivers: [
+    "Port Harcourt",
+    "Obio-Akpor",
+    "Eleme",
+    "Gokana",
+    "Oyigbo",
+  ],
+  Sokoto: [
+    "Sokoto",
+    "Wurno",
+    "Gwadabawa",
+    "Yabo",
+    "Isa",
+  ],
+  Taraba: [
+    "Jalingo",
+    "Wukari",
+    "Bali",
+    "Kumo",
+    "Karim Lamido",
+  ],
+  Yobe: [
+    "Damaturu",
+    "Potiskum",
+    "Gashua",
+    "Nguru",
+    "Geidam",
+  ],
+  Zamfara: [
+    "Gusau",
+    "Kaura Namoda",
+    "Tsafe",
+    "Anka",
+    "Talata Mafara",
+  ],
+};
 
-//this code collectes selected states and display the cites in that state
-selectedOptions.addEventListener('change', ()=>{
-  const  options = selectedOptions.options;
-  for(let i=0; i<options.length; i++){
-    //this part append the selected state cities to the options
-    if(selectedOptions[i].value == 'Lagos'){
-      const Changecities = document.getElementById('cities');
-      Changecities.innerHTML = "";
-      const lagosCities = [
-        "Lagos Island",
-        "Ikeja",
-        "Victoria Island",
-        "Apapa",
-        "Surulere",
-        "Lekki",
-        "Ajah",
-        "Ikorodu",
-        "Badagry",
-        "Banana Island",
-      ]
-      //creating the options
-      
-      for(let a=0; a<lagosCities.length; a++){
-        const optionsTag = document.createElement('option');
-        optionsTag.textContent = lagosCities[a];
-        Changecities.appendChild(optionsTag);
+
+const selectedState = document.getElementById('states');
+const cities = document.getElementById('cities');
+//adding an eventlistener
+selectedState.addEventListener('change', ()=>{
+  //this part iterate over variable named nigeriaCities
+  for(const key in nigeriaCities){
+    //check if selected value equals to one of the nigeriaCities varable
+    //create an option tag
+    //append the values under selected state to...
+    if(selectedState.value == key){
+     for(let i=0; i<nigeriaCities[key].length; i++){
+      cities.innerHTML = "";
+      for(let a = 0; a<nigeriaCities[key].length; a++){
+        const optionTag = document.createElement('option');
+        optionTag.textContent = nigeriaCities[key][a];
+        cities.appendChild(optionTag);
       }
-      
-
+     }
     }
   }
-  
-  
+
 })
-
-
-
-
-//console.log(selectedOptions.value);
-//const h3Tag = document.createElement('h3'); 
-//h3Tag.innerHTML = selectedOptions[0].value;
-
-//document.querySelector('.section').appendChild(h3Tag);

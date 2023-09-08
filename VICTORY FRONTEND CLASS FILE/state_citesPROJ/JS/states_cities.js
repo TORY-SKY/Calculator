e2f20ead1b7dee9a9c3a93441e976464c6e01d84
TORY-SKY <ezevictor559@gmail.com>
2023-09-08 03:32:53 -0700

    feat: Implement dynamic city selection based on the chosen state
    This commit introduces JavaScript code that allows users to select a state from a dropdown menu. Upon selecting a state, the code dynamically populates another dropdown menu with cities specific to that state.
    
    The key features of this code include:
    - Retrieving the selected state from the dropdown menu.
    - Clearing the city options when a new state is selected.
    - Populating the city dropdown menu with cities corresponding to the selected state (e.g., Lagos in this case).
    
    This functionality enhances the user experience by providing relevant city options based on the chosen state, making it easier to navigate and select cities.
    
    This code demonstrates a practical use of JavaScript event listeners and dynamic DOM manipulation, which can be valuable for building interactive web applications.

--- VICTORY FRONTEND CLASS FILE/state_citesPROJ/JS/states_cities.js
+++ VICTORY FRONTEND CLASS FILE/state_citesPROJ/JS/states_cities.js
@@ -1,19 +1,46 @@
-const selectedOptions = document.querySelectorAll('select');
-selectedOptions[0].forEach(element=>(){
-    if(element == 'Adamawa'){
-        alert(element);
-    }
-});
-
-
-
-
-
-
-
-
+const selectedOptions = document.getElementById('states');
 console.log(selectedOptions.value);
-const h3Tag = document.createElement('h3');
-h3Tag.innerHTML = selectedOptions[0].value;
 
-document.querySelector('.section').appendChild(h3Tag);
+//this code collectes selected states and display the cites in that state
+selectedOptions.addEventListener('change', ()=>{
+  const  options = selectedOptions.options;
+  for(let i=0; i<options.length; i++){
+    //this part append the selected state cities to the options
+    if(selectedOptions[i].value == 'Lagos'){
+      const Changecities = document.getElementById('cities');
+      Changecities.innerHTML = "";
+      const lagosCities = [
+        "Lagos Island",
+        "Ikeja",
+        "Victoria Island",
+        "Apapa",
+        "Surulere",
+        "Lekki",
+        "Ajah",
+        "Ikorodu",
+        "Badagry",
+        "Banana Island",
+      ]
+      //creating the options
+      
+      for(let a=0; a<lagosCities.length; a++){
+        const optionsTag = document.createElement('option');
+        optionsTag.textContent = lagosCities[a];
+        Changecities.appendChild(optionsTag);
+      }
+      
+
+    }
+  }
+  
+  
+})
+
+
+
+
+//console.log(selectedOptions.value);
+//const h3Tag = document.createElement('h3'); 
+//h3Tag.innerHTML = selectedOptions[0].value;
+
+//document.querySelector('.section').appendChild(h3Tag);

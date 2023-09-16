0437cef71955e7e034e06b9589529f1a2940b437
TORY-SKY <ezevictor559@gmail.com>
2023-09-16 15:57:56 -0700

    boot

--- VICTORY FRONTEND CLASS FILE/JAVASCRIPT/JSON.js
+++ VICTORY FRONTEND CLASS FILE/JAVASCRIPT/JSON.js
@@ -1,5 +1,17 @@
 //Initialize tooltips
-var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
-var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
-  return new bootstrap.Tooltip(tooltipTriggerEl)
-})
+//var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
+//var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+ // return new bootstrap.Tooltip(tooltipTriggerEl)
+//})
+
+function duplicateEncode(){
+  // ...
+var Success = "Success";
+var charArray = Success.split('');
+for(x in charArray){
+  if(x.value == x.value ){
+    alert(charArray[x].length);
+  }
+}
+
+}
